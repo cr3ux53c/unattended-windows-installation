3e6b60bedf57bbfc2fd9533671dc7464354fdddc
LIM HYUN SEOK <cr3ux53c@gmail.com>
2020-01-19 23:40:39 +0900

    문자 버그 수정

--- USB/script.cmd
+++ USB/script.cmd
@@ -1,8 +1,8 @@
 @call X:\sources\header.cmd
 wpeinit
 diskpart /s X:\sources\diskpart.txt
-for %%a in (A: B: C: D: E: F: G: H: I: J: K: L: M: N: O: P: Q: R: S: T: U: V: W: Z:) do (
-    if exist %%a\CDUsb.y (
+for %%a in (A B C D E F G H I J K L M N O P Q R S T U V W Z) do (
+    if exist %%a:\CDUsb.y (
         set strDestDrive=%%a
     )
 )

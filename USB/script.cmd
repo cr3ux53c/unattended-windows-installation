0fdff02d10ec978c281157d5e27577cc3e03e5b1
LIM HYUN SEOK <cr3ux53c@gmail.com>
2020-01-19 22:58:04 +0900

    WDS, USB응으료 나눔

---
+++ USB/script.cmd
@@ -0,0 +1,10 @@
+@call header.cmd
+wpeinit
+diskpart /s X:\sources\diskpart.txt
+for %%a in (A: B: C: D: E: F: G: H: I: J: K: L: M: N: O: P: Q: R: S: T: U: V: W: Z:) do (
+    if exist %%a\CDUsb.y (
+        set strDestDrive=%%a
+    )
+)
+dism /Apply-Image /ImageFile:%strDestDrive%:\sources\install.esd /Index:1 /ApplyDir:W:\
+bcdboot W:\windows /l ko-kr /s S:

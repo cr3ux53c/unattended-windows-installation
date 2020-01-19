ac3579994713f59c67990163a5cb0eca2e77651c
LIM HYUN SEOK <cr3ux53c@gmail.com>
2020-01-20 00:53:32 +0900

    Display WIMInfo before DISM Apply

--- USB/script.cmd
+++ USB/script.cmd
@@ -7,5 +7,6 @@ for %%a in (A B C D E F G H I J K L M N O P Q R S T U V W Z) do (
         set strDestDrive=%%a
     )
 )
+dism /Get-WIMInfo /WIMFile:%strDestDrive%:\sources\install.esd /Index:1
 dism /Apply-Image /ImageFile:%strDestDrive%:\sources\install.esd /Index:1 /ApplyDir:W:\
 bcdboot W:\windows /l ko-kr /s S:

646102a2c4b5331f3f350d67ca04aaa635b9f74b
LIM HYUN SEOK <cr3ux53c@gmail.com>
2020-01-19 23:28:16 +0900

    header.cmd 못찾는 버그 수정

--- USB/script.cmd
+++ USB/script.cmd
@@ -1,4 +1,4 @@
-@call header.cmd
+@call X:\sources\header.cmd
 wpeinit
 diskpart /s X:\sources\diskpart.txt
 for %%a in (A: B: C: D: E: F: G: H: I: J: K: L: M: N: O: P: Q: R: S: T: U: V: W: Z:) do (
--- WDS/script.cmd
+++ WDS/script.cmd
@@ -1,4 +1,4 @@
-@call header.cmd
+@call X:\sources\header.cmd
 wpeinit
 set /p server_ip=Input WDS Server IP address: 
 net use N: \\%server_ip%\PXEShare

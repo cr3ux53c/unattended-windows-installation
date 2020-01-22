c69b0cfcee16cb65fcb0214e57ccd61bbd42b874
LIM HYUN SEOK <cr3ux53c@gmail.com>
2020-01-22 23:17:07 +0900

    Fixed string bug

--- PXE/PXEServer/autounattend.cmd
+++ PXE/PXEServer/autounattend.cmd
@@ -1,5 +1,5 @@
 start taskmgr
-diskpart /s N:\diskpart-mbr.cfg
+diskpart /s N:\diskpart.mbr.cfg
 dism /Get-WIMInfo /WIMFile:N:\install-unattend-KNUT_Software_2020.esd /Index:1
 dism /Apply-Image /ImageFile:N:\install-unattend-KNUT_Software_2020.esd /Index:1 /ApplyDir:W:\
 bcdboot W:\windows /l ko-kr /s S:

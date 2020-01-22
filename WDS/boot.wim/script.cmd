bb246df87bc4c67d908843b7831cafa2c7898790
LIM HYUN SEOK <cr3ux53c@gmail.com>
2020-01-22 23:09:16 +0900

    PXE 공유 디렉터리 자동 로그온

--- WDS/boot.wim/script.cmd
+++ WDS/boot.wim/script.cmd
@@ -1,5 +1,6 @@
 @echo off
 wpeinit
-set /p server_ip=Input WDS Server IP address: 
-net use N: \\%server_ip%\PXEShare
+cscript //nologo X:\sources\wds_server_ip.vbs > X:\Windows\wds_server_ip.log
+set /p serverIpAddress=<X:\Windows\wds_server_ip.log
+net use N: \\%serverIpAddress%\PXEShare /user:WDSUser 6dEYN2HTvJ63xqkS
 call N:\autounattend.cmd

d7e16be98da2bdd7c391450025f2a8d164ddb704
LIM HYUN SEOK <cr3ux53c@gmail.com>
2020-01-23 01:14:23 +0900

    서식 적용

--- PXE/PXEServer/autounattend.cmd
+++ PXE/PXEServer/autounattend.cmd
@@ -1,5 +1,32 @@
+::Initialize Environment
+title loopback.kr(TM) Windows Autounattend Script 2020
+color 1F
+
 start taskmgr
-diskpart /s N:\diskpart.mbr.cfg
-dism /Get-WIMInfo /WIMFile:N:\install-unattend-KNUT_Software_2020.esd /Index:1
-dism /Apply-Image /ImageFile:N:\install-unattend-KNUT_Software_2020.esd /Index:1 /ApplyDir:W:\
+echo.
+echo ==============================================================
+echo.
+echo Setting up disk partitions...
+echo.
+diskpart /s N:\diskpart.gpt.cfg
+
+echo.
+echo ==============================================================
+echo.
+echo Information about Windows image to be installed:
+echo.
+dism /Get-WIMInfo /WIMFile:N:\install.wim /Index:1
+
+echo.
+echo ==============================================================
+echo.
+echo Apply Windows image...
+echo.
+dism /Apply-Image /ImageFile:N:\install.wim /Index:1 /ApplyDir:W:\
+
+echo.
+echo ==============================================================
+echo.
+echo Setting up bootloader...
+echo.
 bcdboot W:\windows /l ko-kr /s S:

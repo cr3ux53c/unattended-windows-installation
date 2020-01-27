b7214513975c1b172df26c511bdda4ae0978762d
LIM HYUN SEOK <cr3ux53c@gmail.com>
2020-01-28 01:04:20 +0900

    Update script

--- ISO/autounattend.cmd
+++ ISO/autounattend.cmd
@@ -1,16 +1,19 @@
-::Initialize Environment
 @echo off
-title loopback.kr(TM) Windows Autounattend Script 2020
+title loopback.kr(TM) Windows Autounattend Installation Script
 color 1F
 
 start /min taskmgr
+
+echo The Autounattend installation will start soon.
+cscript //nologo %1:\sources\sleep.vbs
+
 echo.
 echo ============================================================
 echo.
 echo Setting up disk partitions...
 echo.
 echo ============================================================
-diskpart /s %1:\diskpart.gpt.cfg
+diskpart /s %1:\sources\diskpart.mbr.cfg
 
 echo.
 echo ============================================================
@@ -31,8 +34,28 @@ dism /Apply-Image /ImageFile:%1:\sources\install.wim /Index:1 /ApplyDir:W:\
 echo.
 echo ============================================================
 echo.
+echo Apply OOBE components...
+echo.
+echo ============================================================
+copy %1:\sources\unattend.xml W:\Windows\Panther\unattend.xml
+copy %1:\sources\Professional-x64.cab W:\Windows\Setup\Professional-x64.cab
+copy %1:\sources\oemlogo.bmp W:\Windows
+
+
+echo.
+echo ============================================================
+echo.
 echo Setting up bootloader...
 echo.
 echo ============================================================
-bcdboot W:\windows /l ko-kr /s S:
+bcdboot W:\windows /l ko-kr /s S: /f BIOS
+
+echo.
+echo ============================================================
+echo.
+echo The system will be rebooted soon.
+echo.
+echo ============================================================
+color 5F
+cscript //nologo %1:\sources\sleep.vbs
 exit
--- PXEShare/autounattend.cmd
+++ PXEShare/autounattend.cmd
@@ -1,16 +1,19 @@
-::Initialize Environment
 @echo off
-title loopback.kr(TM) Windows Autounattend Script 2020
+title loopback.kr(TM) Windows Autounattend Installation Script
 color 1F
 
 start /min taskmgr
+
+echo The Autounattend installation will start soon.
+cscript //nologo %1:\sources\sleep.vbs
+
 echo.
 echo ============================================================
 echo.
 echo Setting up disk partitions...
 echo.
 echo ============================================================
-diskpart /s N:\diskpart.gpt.cfg
+diskpart /s %1:\sources\diskpart.mbr.cfg
 
 echo.
 echo ============================================================
@@ -18,7 +21,7 @@ echo.
 echo Information about Windows image to be installed:
 echo.
 echo ============================================================
-dism /Get-WIMInfo /WIMFile:N:\install.wim /Index:1
+dism /Get-WIMInfo /WIMFile:%1:\sources\install.wim /Index:1
 
 echo.
 echo ============================================================
@@ -26,7 +29,18 @@ echo.
 echo Apply Windows image...
 echo.
 echo ============================================================
-dism /Apply-Image /ImageFile:N:\install.wim /Index:1 /ApplyDir:W:\
+dism /Apply-Image /ImageFile:%1:\sources\install.wim /Index:1 /ApplyDir:W:\
+
+echo.
+echo ============================================================
+echo.
+echo Apply OOBE components...
+echo.
+echo ============================================================
+copy %1:\sources\unattend.xml W:\Windows\Panther\unattend.xml
+copy %1:\sources\Professional-x64.cab W:\Windows\Setup\Professional-x64.cab
+copy %1:\sources\oemlogo.bmp W:\Windows
+
 
 echo.
 echo ============================================================
@@ -34,5 +48,14 @@ echo.
 echo Setting up bootloader...
 echo.
 echo ============================================================
-bcdboot W:\windows /l ko-kr /s S:
+bcdboot W:\windows /l ko-kr /s S: /f BIOS
+
+echo.
+echo ============================================================
+echo.
+echo The system will be rebooted soon.
+echo.
+echo ============================================================
+color 5F
+cscript //nologo %1:\sources\sleep.vbs
 exit
